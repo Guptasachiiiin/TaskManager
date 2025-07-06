5c16fb837f588a143f7ca7f880cd0cf484baa6a5
sachin gupta <sachingupta176477@gmail.com>
2025-07-07 02:02:13 +0530

    backend json update

--- backend/index.js
+++ backend/index.js
@@ -1,30 +1,31 @@
 import express from "express";
-import cors from "cors"
-import connection from "./database/db";
+import cors from "cors";
+import connection from "./database/db.js";
 import { configDotenv } from "dotenv";
-import userRoute from "./routes/user.route";
-import taskRoute from "./routes/task.route";
-configDotenv()
+import userRoute from "./routes/user.route.js";
+import taskRoute from "./routes/task.route.js";
+configDotenv();
 
-const port=process.env.PORT || 9000
-const app=express();
+const port = process.env.PORT || 9000;
+const app = express();
 
-app.use(cors({
-  origin: 'http://localhost:5173', // Allow frontend on localhost:5174
-  methods: ['GET', 'POST','PUT','DELETE'], // Specify allowed methods
-  allowedHeaders: ['Content-Type', 'Authorization'], // Specify allowed headers
-}));
+app.use(
+  cors({
+    origin: "http://localhost:5173", // Allow frontend on localhost:5174
+    methods: ["GET", "POST", "PUT", "DELETE"], // Specify allowed methods
+    allowedHeaders: ["Content-Type", "Authorization"], // Specify allowed headers
+  })
+);
 
-
-app.use(express.json())
-connection(process.env.MONGO_URI)  // database connection
+app.use(express.json());
+connection(process.env.MONGO_URI); // database connection
 
 app.get("/", (req, res) => {
-    res.send("server is running...........");
-  }); 
-app.listen(port,()=>{
-    console.log("server is running on  http://localhost:" + port)
-})
+  res.send("server is running...........");
+});
+app.listen(port, () => {
+  console.log("server is running on  http://localhost:" + port);
+});
 
-app.use("/api/auth",userRoute)  //auth route
-app.use("/api",taskRoute)   //task route
+app.use("/api/auth", userRoute); //auth route
+app.use("/api", taskRoute); //task route
--- backend/routes/task.route.js
+++ backend/routes/task.route.js
@@ -1,33 +1,33 @@
 import express from "express";
-import { DeleteTask, GetTask, Task, UpdateTask } from "../controllers/task.controller";
-import { auth } from "../middleware/auth.middleware";
-import { taskValidation } from "../validators/auth.validator";
+import {
+  DeleteTask,
+  GetTask,
+  Task,
+  UpdateTask,
+} from "../controllers/task.controller.js";
+import { auth } from "../middleware/auth.middleware.js";
+import { taskValidation } from "../validators/auth.validator.js";
 import { validationResult } from "express-validator";
 
 const route = express.Router();
 //  route.post("/task",auth,Task)
 
-
 //express validator response
-  const validate = (req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(422).json({
-        success: false,
-        message: "Validation failed.",
-        errors: errors.array(),
-      });
-    }
-    next();
-  };
+const validate = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      success: false,
+      message: "Validation failed.",
+      errors: errors.array(),
+    });
+  }
+  next();
+};
 
-
- route.post("/task", auth, taskValidation, validate, Task);
- route.get("/task",auth,GetTask)
- route.delete("/task/:task_id",auth,DeleteTask)
- route.put("/task/:task_id",auth,UpdateTask)
-
-
-
+route.post("/task", auth, taskValidation, validate, Task);
+route.get("/task", auth, GetTask);
+route.delete("/task/:task_id", auth, DeleteTask);
+route.put("/task/:task_id", auth, UpdateTask);
 
 export default route;
